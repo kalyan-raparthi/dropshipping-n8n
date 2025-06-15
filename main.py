f28cb9995f8bc92df10fa92de74ce778513f5eb4
qb <kalyan.raparthi@hotmail.com>
2025-06-15 22:48:10 +0530

    track.html is added

--- main.py
+++ main.py
@@ -4,7 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')  # Make sure templates/index.html exists
+    return render_template('index.html') 
 
+
+@app.route('/track')
+def track():
+    return render_template('track.html') 
 if __name__ == '__main__':
     app.run(debug=True)
